b01ac037056c086e10b4325e9d0c15ad781be92f
Batsiev Oleg <batsiev2006@gmail.com>
2023-05-18 23:07:48 +0300

    Start using boosters

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,44 +1,58 @@
 using System;
 using System.IO;
 using UnityEngine;
+using System.Collections;
+
 
 public class Move : MonoBehaviour
 {
+    public float speed = 0f; // Скорость
+    public float maxSpeed = 50f; // Максимальная скорость
+    public float sideSpeed = 0f; // Боковая скорость
+    public float rotateSpeed = 50f; // Скорость поворота
 
-    public float speed = 0f; //Скорость
-    public float maxSpeed = 50f; //Максимальная скорость
-    public float sideSpeed = 0f; //Боковая скорость
-    public float rotateSpeed = 50f; //Скорость поворота
+    public float acceleration = 0.01f; // Ускорение
+    public float deceleration = 0.02f; // Замедление
 
-    public float scores = 0f; 
-    public float highScore = 0f; 
+    public float liftedObjectHeight = 2f; // Высота поднятия объекта
+    public float liftedObjectDuration = 2f; // Продолжительность поднятия объекта
 
-
-    void Update()    
+    void Update()
     {
-        float moveSide = Input.GetAxis("Horizontal"); 
-        float moveForward = Input.GetAxis("Vertical"); 
-        float rotation = moveSide * rotateSpeed; 
+        float moveSide = Input.GetAxis("Horizontal");
+        float moveForward = Input.GetAxis("Vertical");
+        float rotation = moveSide * rotateSpeed;
 
-        if (moveForward != 0)
+        if (moveForward > 0)
         {
-            speed += 0.01f * moveForward; 
+            speed += acceleration * moveForward;
         }
-        else 
+        else if (moveForward < 0)
         {
             if (speed > 0)
             {
-                speed -= 0.01f;
+                speed += deceleration * moveForward;
             }
             else
             {
-                speed += 0.01f;
+                speed += acceleration * moveForward;
+            }
+        }
+        else
+        {
+            if (speed > 0)
+            {
+                speed -= deceleration;
+            }
+            else if (speed < 0)
+            {
+                speed += deceleration;
             }
         }
 
-        if (speed > maxSpeed)
+        if (Mathf.Abs(speed) > maxSpeed)
         {
-            speed = maxSpeed; 
+            speed = Mathf.Sign(speed) * maxSpeed;
         }
 
         MoveCar(rotation);
@@ -46,8 +60,43 @@ public class Move : MonoBehaviour
 
     void MoveCar(float rotation)
     {
-        transform.Rotate(0, rotation * Time.deltaTime, 0); 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bo_up"))
+        {
+            speed += acceleration * 5;
+            LiftAndRemoveObject(other.gameObject);
+        }
+        else if (other.CompareTag("Bo_down"))
+        {
+            speed -= acceleration * 5;
+            LiftAndRemoveObject(other.gameObject);
+        }
+    }
+
+    void LiftAndRemoveObject(GameObject obj)
+    {
+        StartCoroutine(LiftAndRemoveRoutine(obj));
+    }
+
+    IEnumerator LiftAndRemoveRoutine(GameObject obj)
+    {
+        Vector3 originalPosition = obj.transform.position;
+        Vector3 targetPosition = originalPosition + new Vector3(0f, liftedObjectHeight, 0f);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < liftedObjectDuration)
+        {
+            float t = elapsedTime / liftedObjectDuration;
+            obj.transform.position = Vector3.Lerp(originalPosition, targetPosition, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(obj);
+    }
 }
