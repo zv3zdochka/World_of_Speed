2c454f85af421f89a04683583fa622985e682a82
Batsiev Oleg <batsiev2006@gmail.com>
2023-05-26 14:19:47 +0300

    Add shop

---
+++ Assets/Scripts/Shoper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Shoper : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0f, rotation, 0f);
+
+        Vector3 forwardMovement = transform.forward * verticalInput * speed * Time.deltaTime;
+        Vector3 newPosition = transform.position + forwardMovement;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/clc.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class clc : MonoBehaviour
+{
+    public void back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
